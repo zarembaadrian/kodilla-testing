cd6177118797c260ac60fde09a2c37c36f8ab9e1
zarembaadrian <zarembaadrian@icloud.com>
2017-12-05 22:11:55 +0000

    changes2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,6 +1,5 @@
 package com.kodilla.testing;
-import  com.kodilla.testing.user.SimpleUser;
-import com.kodilla.testing.user.calculator.CalculatorApi;
+import com.kodilla.testing.calculator.CalculatorApi;
 
 public class TestingMain {
     public static void main (String[]args) {
