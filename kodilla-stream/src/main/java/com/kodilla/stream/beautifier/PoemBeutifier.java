aed5924bb61269da7ab51f7ea73f03c20ff85e82
zarembaadrian <zarembaadrian@icloud.com>
2018-01-13 15:27:35 +0000

    Project1

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeutifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeutifier.java
@@ -1,8 +1,9 @@
 package com.kodilla.stream.beautifier;
 
 public class PoemBeutifier {
-    public void beautify(String abc, PoemDecorator poemDecorator) {
+    public String beautify(String abc, PoemDecorator poemDecorator) {
         String result = poemDecorator.decorate(abc);
         System.out.println("Result after beautify: " + result);
+        return null;
     }
 }
